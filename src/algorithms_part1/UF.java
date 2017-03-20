0f202b12258e06e5862239ea9d660c58bab59152
Kazuhiro Oinuma <oinume@gmail.com>
2017-03-21 03:56:36 +0900

    Union Find

---
+++ src/algorithms_part1/UF.java
@@ -0,0 +1,13 @@
+package algorithms_part1;
+
+public class UF {
+    public UF(int n) {
+    }
+
+    public void union(int p, int q) {
+    }
+
+    public boolean connected(int p, int q) {
+        return false;
+    }
+}
