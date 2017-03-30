ece2c87ea2ccc7c324dd31e33061e2240509fccf
Kazuhiro Oinuma <oinume@gmail.com>
2017-03-30 10:04:14 +0900

    Add interface

---
+++ src/main/java/algorithm/union_find/UnionFindTest.java
@@ -0,0 +1,9 @@
+package algorithm.union_find;
+
+/**
+ * Created by kazuhiro on 2017/03/30.
+ */
+public class UnionFindTest {
+
+
+}
---
+++ src/test/java/algorithm/union_find/UF.java
@@ -0,0 +1,9 @@
+package algorithm.union_find;
+
+public interface UF {
+
+    public boolean connected(int p, int q);
+
+    public void union(int p, int q);
+
+}
