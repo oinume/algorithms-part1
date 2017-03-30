7cf46588d7f9fab52b6c9e8332e15a96218cdfcb
Kazuhiro Oinuma <oinume@gmail.com>
2017-03-30 23:10:01 +0900

    Implement union and connected methods

---
+++ src/main/java/algorithm/union_find/UFQuickFind.java
@@ -0,0 +1,38 @@
+package algorithm.union_find;
+
+import java.util.Arrays;
+
+/**
+ * Created by kazuhiro on 2017/03/30.
+ */
+public class UFQuickFind implements UF {
+
+    private int[] data;
+
+    public UFQuickFind(int n) {
+        data = new int[n];
+        for (int i = 0; i < n; i++) {
+            data[i] = i;
+        }
+    }
+
+    @Override
+    public boolean connected(int p, int q) {
+        return data[p] == data[q];
+    }
+
+    @Override
+    public void union(int p, int q) {
+        data[p] = q;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == p) {
+                data[i] = q;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(data);
+    }
+}
---
+++ src/test/java/algorithm/union_find/UFQuickFindTest.java
@@ -0,0 +1,21 @@
+package algorithm.union_find;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by kazuhiro on 2017/03/30.
+ */
+public class UFQuickFindTest {
+
+    @Test
+    public void testBasicUnion() {
+        UF uf = new UFQuickFind(10);
+        assertEquals(false, uf.connected(0, 1));
+
+        uf.union(0, 1);
+        assertEquals(true, uf.connected(0, 1));
+    }
+
+}
