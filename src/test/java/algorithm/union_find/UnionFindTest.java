2d6ca5eaf638ecc66276fbbd19db3c875dc03f5b
Kazuhiro Oinuma <oinume@gmail.com>
2017-03-30 10:27:19 +0900

    Generate Intellij stuff from gradle

--- src/test/java/algorithm/union_find/UnionFindTest.java
+++ src/test/java/algorithm/union_find/UnionFindTest.java
@@ -1,9 +1,15 @@
 package algorithm.union_find;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 /**
  * Created by kazuhiro on 2017/03/30.
  */
 public class UnionFindTest {
 
+    @Test
+    public void testUnionFind() {
+    }
 
 }
